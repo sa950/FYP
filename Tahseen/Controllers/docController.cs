ac1bd62859dab163a5b779c064b2afa6d28c98b8
sa950 <lora112244sma@gmail.com>
2022-02-13 20:28:11 +0300

    doctor foilder
    commit doc

---
+++ Tahseen/Controllers/docController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tahseen.Controllers
+{
+    public class docController : Controller
+    {
+        // GET: doc
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult files()
+        {
+            return View();
+        }
+        public ActionResult cdata()
+        {
+            return View();
+        }
+
+
+
+    }
+}
